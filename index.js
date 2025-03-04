637b0a9ad0a35aab44c203b8d4fcc604aebe8dde
Stephen <hellostephenwm@gmail.com>
2025-03-03 20:48:01 -0500

    feat: add mutations and mutation resolvers for creating new phonebook entries

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v4: uuid } = require("uuid");
+const { GraphQLError } = require("graphql");
 
 let persons = [
 	{
@@ -42,6 +44,15 @@ const typeDefs = `
         allPersons: [Person!]!
         findPerson(name: String!): Person
     }
+
+    type Mutation {
+        addPerson(
+            name: String!
+            phone: String
+            street: String!
+            city: String!
+        ): Person
+    }
 `;
 
 const resolvers = {
@@ -56,6 +67,22 @@ const resolvers = {
 				street: root.street,
 				city: root.city,
 			};
+		},
+	},
+	Mutation: {
+		addPerson: (root, args) => {
+			if (persons.find((p) => p.name === args.name)) {
+				throw new GraphQLError("Name must be unique", {
+					extensions: {
+						code: "BAD_USER_INPUT",
+						invalidArgs: args.name,
+					},
+				});
+			}
+
+			const person = { ...args, id: uuid() };
+			persons = persons.concat(person);
+			return person;
 		},
 	},
 };
